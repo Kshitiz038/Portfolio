081d1d96dfd06dd3a92a593379abe6eaf6408c1e
saiki038 <saxenaktz@gmail.com>
2024-12-03 17:46:21 +0530

    updated version with enhnaced styles

--- script1.js
+++ script1.js
@@ -1,10 +1,10 @@
 // my text which are going to show on the page //
 var typed1 = new Typed('#elements',{
-    strings: ['My name is Kshitiz Saxena and this is my portfolio. My passion is'],
+    strings: ['Passion'],
     typeSpeed: 150,
     onComplete: function () {
-        var typed2 = new Typed('#element',{
-            strings: [' Web developing', 'Graphic designing', 'Web designing'],
+var typed2 = new Typed('#element',{
+            strings: ['Web developing', 'Graphic designing', 'Web designing'],
             typeSpeed: 100,
         });    
     },
