02d06a77db8b4e1cf212a3d78b07c659783a2780
saiki038 <saxenaktz@gmail.com>
2024-12-04 16:23:11 +0530

    New Device styles and modified CSS

--- script1.js
+++ script1.js
@@ -4,10 +4,14 @@ var typed1 = new Typed('#elements',{
     typeSpeed: 150,
     onComplete: function () {
 var typed2 = new Typed('#element',{
-            strings: ['Web developing', 'Graphic designing', 'Web designing'],
+            strings: [
+                'Web developing', 
+                'Graphic designing', 
+                'Web designing'
+            ],
             typeSpeed: 100,
+            loop: true
         });    
     },
 });
-
    
